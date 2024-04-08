74456b30b2132e6e83de1be6f3ea6e63b62afd70
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2024-04-08 12:35:16 +0530

    adding new file

---
+++ Find_Sum.js
@@ -0,0 +1,42 @@
+function findSum(a,b){
+    let sum=(a*a)+(b*b)+2*a*b
+    return sum;
+
+}
+console.log(findSum(5,10))
+
+
+
+function findSub(a,b){
+    let sub=(a*a)-(b*b)+2*a*b
+    return sub;
+
+}
+console.log(findSub(5,10))
+
+
+
+function sumOfNumber(a,b){
+    let sum=(a*a*a)+(b*b*b)+(3*a*a)*b+(3*b*b)*a
+    return sum;
+}
+console.log(sumOfNumber(5,10))
+
+
+
+
+function printTable(){
+    // let num=2
+for(i=1;i<=10;i++){
+    // console.log(`${num}*${i}=${num*i}`)
+    console.log(i)
+}
+}
+console.log(printTable())
+
+function table(num){
+    for(num=1;num<=10;num+1){
+
+    }
+}
+console.log(table(3))
--- functionTest.js
+++ functionTest.js
@@ -53,44 +53,78 @@ function findIntrest() {
     let simpleIntrest = principalAmount * rate * time / 100
     return simpleIntrest;
 }
-// console.log(findIntrest())
+ console.log(findIntrest())
 
 function getIntrest(pricipalAmt, rt, tm) {
     let simpleIntrest1 = pricipalAmt * rt * tm / 100
     return simpleIntrest1
 }
-// console.log(getIntrest(500000,10,2))
+console.log(getIntrest(500000,10,2))
 
 
 function findAreaOfTriangle() {
     let height = 100;
     let base = 120;
-    let areaOfTraingle = 1/2 * base * height
+    let areaOfTraingle = 1 / 2 * base * height
     return areaOfTraingle
 }
 console.log(findAreaOfTriangle())
 
-function getAreaOfTraingle(base,height){
-let areaOfTraingle=1/2*base*height
-console.log(areaOfTraingle)
+function getAreaOfTraingle(base, height) {
+    let areaOfTraingle = 1 / 2 * base * height
+    console.log(areaOfTraingle)
 }
-getAreaOfTraingle(100,50)
+// getAreaOfTraingle(100,50)
 
 
-function getAreaOfTraingle1(base,height){
-    let areaOfTraingle=1/2*base*height
+function getAreaOfTraingle1(base, height) {
+    let areaOfTraingle = 1 / 2 * base * height
     return areaOfTraingle
-    }
-    console.log(getAreaOfTraingle1(100,70))
+}
+console.log(getAreaOfTraingle1(100, 70))
 
-  function findAreaOfCircle(){
+function findAreaOfCircle() {
     // a=pi*r*r
-    const pi=3.14;
-    let radius=5;
-    let areaOfCircle=pi*radius*radius
+    const pi = 3.14;
+    let radius = 5;
+    let areaOfCircle = pi * radius * radius
     return areaOfCircle
-  }  
-  console.log(findAreaOfCircle())
+}
+//   console.log(findAreaOfCircle())
+
+
+function getTemp() {
+    //fahrenheit=degree celsius * 9/5 +32
+    let degreeCelsius = 41
+    let fahrenheit = degreeCelsius * (9 / 5) + 32
+    return fahrenheit
+
+}
+console.log(getTemp())
+
+
+function findTemp(degreeCelsius) {
+    //fahrenheit=degree celsius * 9/5 +32
+    
+    let fahrenheit = degreeCelsius * (9 / 5) + 32
+    return fahrenheit
+
+}
+console.log(getTemp(41))
+
+
+function Temp() {
+    //celsius=fahrenheit-32*5/9
+    let fahrenheit=105.8
+    let celsius = (fahrenheit -32) *5/9
+    return celsius
+
+}
+console.log(Temp())
+
+
+
+
 
 
 
