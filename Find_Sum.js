ea077fd0bb41c0d8f72bc00894fffb72a825d9e3
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2024-04-09 00:39:52 +0530

    adding

--- Find_Sum.js
+++ Find_Sum.js
@@ -3,7 +3,7 @@ function findSum(a,b){
     return sum;
 
 }
-console.log(findSum(5,10))
+// console.log(findSum(5,10))
 
 
 
@@ -12,7 +12,7 @@ function findSub(a,b){
     return sub;
 
 }
-console.log(findSub(5,10))
+// console.log(findSub(5,10))
 
 
 
@@ -20,24 +20,30 @@ function sumOfNumber(a,b){
     let sum=(a*a*a)+(b*b*b)+(3*a*a)*b+(3*b*b)*a
     return sum;
 }
-console.log(sumOfNumber(5,10))
+// console.log(sumOfNumber(5,10))
 
 
 
 
 function printTable(){
-     let num=2
+     let num=5
+     let temp=[];
+
 for(i=1;i<=10;i++){
-    console.log(`${num}*${i}=${num*i}`)
-   
+  
+    temp.push(`${num}*${i}=${num*i}`)
 }
+return temp;
 }
-printTable()
+
+console.log(printTable())
+
+
 
 function table(num,j,i){
     for(i;i<=j;i++){
 console.log(`${num}*${i}=${num*i}`)
     }
 }
-table(3,20,5)
-table(5,20,5)
+// table(3,20,5)
+// table(5,20,5)
