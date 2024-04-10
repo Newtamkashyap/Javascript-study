e8d0076ccb48b8a8f875382e61558bcad8789671
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2024-04-10 18:18:28 +0530

    odd/even number

---
+++ find_oddeven_number.js
@@ -0,0 +1,27 @@
+function findEven(){
+    let num=90;
+    if(num%2==0){
+        return "This is even number"
+
+    }
+    else{
+        return "This is odd number"
+    }
+
+}
+console.log(findEven())
+
+function findEvenNumber(num){
+    
+    if(num%2==0){
+        return "This is even number"
+
+    }
+    else{
+        return "This is odd number"
+    }
+
+}
+console.log(findEvenNumber(87))
+
+
