5d1e4df4fbd25de864ca7da4567604ae7e2f8b09
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2024-04-21 18:30:17 +0530

    simplifyCode

--- 21_April/classExamples.js
+++ 21_April/classExamples.js
@@ -46,25 +46,14 @@ resturant1.foodItem2="Dum_Biryani"//setting the value
 let resturant2=new Resturant();
 console.log("Resturant2_Menu",resturant2)
 
-
 let resturant3=new Resturant();
 resturant3.menu="Non-Vegitarian";
-console.log(resturant3.menu)
-
 resturant3.foodItem1="Chicken";
-console.log(resturant3.foodItem1)
-
 resturant3.foodItem2="Fish";
-console.log(resturant3.foodItem2)
-
 resturant3.foodItem3="Mutton";
-console.log(resturant3.foodItem3)
-
 resturant3.foodItem4="Egg";
-console.log(resturant3.foodItem4)
-
 resturant3.foodItem5="Prawns";
-console.log(resturant3.foodItem5)
+console.log("Resturant3_Items",resturant3)
 
 console.log(resturant1.VegItems())
 console.log(resturant2.nonVegItems())
