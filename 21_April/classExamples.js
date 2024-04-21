353c41cfe690dbc0ebc0fa121c8bc8d2e8e2d5e5
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2024-04-21 17:28:33 +0530

    class_programs

---
+++ 21_April/classExamples.js
@@ -0,0 +1,36 @@
+class Mobile{
+    mobileName;
+    mobileColor;
+    mobileCamera;
+    mobileHeight;
+    mobileWidth;
+    mobileFlashLight;
+}
+
+/* new Mobile()
+console.log(new Mobile())
+console.log(new Mobile)
+
+//OR
+
+ let mobile=new Mobile();
+console.log(mobile)*/
+
+class Resturant{
+    menu="Vegitarian";
+    foodItem1="Biryani";
+    foodItem2="curry_Chawal";
+    foodItem3="Rajma_Chawal";
+    foodItem4="Panner";
+    foodItem5="Paneer_Tikka";
+
+    
+    
+}
+let resturant=new Resturant();
+console.log(resturant.foodItem4);
+resturant.foodItem2="Dum_Biryani"
+console.log(resturant)
+
+
+
