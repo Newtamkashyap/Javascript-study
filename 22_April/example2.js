a22d7809087af56e341d9121730f7775dd97c950
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2024-04-23 00:44:02 +0530

    loopExamples

---
+++ 22_April/example2.js
@@ -0,0 +1,16 @@
+function table(num){
+    
+    for(starting=1;starting<=10;starting++){
+        console.log(num+"*"+starting+"="+num*starting);
+    }
+}
+ table(5)
+
+function findTable(){
+    let i=0;
+    while(i<=10){
+        console.log(i);
+        i++;
+    };
+}
+findTable()
