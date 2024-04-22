b917e69ce15ddb08f38a4fd2fec79b33ea4d7772
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2024-04-22 19:06:41 +0530

    .

---
+++ 22_April/ifElseExample.js
@@ -0,0 +1,12 @@
+/*18. Write a JavaScript program to check a pair of numbers and 
+return true if one of the numbers is 50 or if their sum is 50.  */
+
+function pairOfNumber(num1, num2) {
+    if (num1 === 50 || num2 === 50) {
+        return "true";
+    }
+    else {
+        return "false";
+    }
+}
+console.log(pairOfNumber(20, 50));
