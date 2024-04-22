ff6dd1a6013e19d596a1b6447b1c524a289bee7f
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2024-04-22 19:09:21 +0530

    adding IfElse program

--- 22_April/ifElseExample.js
+++ 22_April/ifElseExample.js
@@ -2,11 +2,11 @@
 return true if one of the numbers is 50 or if their sum is 50.  */
 
 function pairOfNumber(num1, num2) {
-    if (num1 === 50 || num2 === 50) {
+    if (num1 === 50 || num2 === 50 ||num1+num2===50) {
         return "true";
     }
     else {
         return "false";
     }
 }
-console.log(pairOfNumber(20, 50));
+console.log(pairOfNumber(10, 30));
