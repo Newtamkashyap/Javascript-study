bcb33151397045d33208b6a1bccd7fee6cf6c065
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2024-04-22 18:37:20 +0530

    adding temprature program

--- 22_April/temprature.js
+++ 22_April/temprature.js
@@ -4,4 +4,19 @@ Expected Output :
 60°C is 140 °F
 45°F is 7.222222222222222°C */
 
+function findCelcius(){
+    // formula:(°F - 32) x 5/9 =°C
+    let f=45;
+    let c=(f - 32) * 5/9 ;
+    return c;
 
+}
+console.log(findCelcius());
+
+function findFahernhite(){
+    //formula: (°C x 9/5) + 32 =°F
+    let c=60;
+    let f= (c * 9/5) + 32
+return f;
+}
+console.log(findFahernhite());
