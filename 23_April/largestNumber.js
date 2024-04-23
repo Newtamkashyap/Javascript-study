9ff59e0e8f3e68d655b5f69058aec017804efa92
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2024-04-23 15:37:19 +0530

    largestnumber_program

---
+++ 23_April/largestNumber.js
@@ -0,0 +1,36 @@
+// 31. Write a JavaScript program to find the largest of three given integers.
+
+function findLargeNumber(){
+    let a=220;
+    let b=302;
+    let c=502;
+    if(a>b){
+        console.log(a," is the largest number");
+    }
+    else if(b>c){
+        console.log(b," is the largest number");
+        
+    }
+    else{
+        console.log(c," is the largest number");
+
+    }
+}
+ findLargeNumber();
+
+function findLargestNumber(a,b,c){
+   
+    if(a>b){
+        console.log(a," is the largest number");
+    }
+    else if(b>c){
+        console.log(b," is the largest number");
+        
+    }
+    
+    else{
+        console.log(c," is the largest number");
+
+    }
+}
+ findLargestNumber(10100,300,2000);
