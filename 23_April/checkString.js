5b0c63313007087786a1f0fb53a8d9568475a621
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2024-04-23 14:03:53 +0530

    checking-string

---
+++ 23_April/checkString.js
@@ -0,0 +1,25 @@
+// 27. Write a JavaScript program to check whether a string starts with 'Java' if it does not otherwise.  
+
+function checkingString() {
+    const str = "javapoint";
+    if (str.startsWith("java")) {
+        console.log(str,"does")
+    }
+    else{
+        console.log("not starts with java")
+    }
+
+
+}
+// checkingString();
+
+function checking(){
+    const elements=["java","python","html","javaPoint","javascript"];
+    for(let i of elements){
+        if(i.startsWith("java")){
+            console.log(i)
+        }
+      
+    }
+}
+checking();
