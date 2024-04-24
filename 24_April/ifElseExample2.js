498a49a46a5203a767676513c2fe8e3ef028f701
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2024-04-24 10:27:09 +0530

    .

---
+++ 24_April/ifElseExample2.js
@@ -0,0 +1,10 @@
+function hospital(medicine){
+    if(medicine==="Dolo"){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+console.log(hospital("Dolo"));
+console.log(hospital("paracetamol"));
