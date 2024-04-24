8df601e2058edb4c51e4ab9b48b288c5d332c2ea
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2024-04-24 13:56:03 +0530

    makedynamic

--- 24_April/classExample.js
+++ 24_April/classExample.js
@@ -3,68 +3,28 @@ class hospital {
     patientAge;
     patientNumber;
     patientRoomNumber;
-    patientStage="critical";
+    patientStage;
 
-    doctor1() {
-        const doctorname = "Dr.Naresh"
-        let PhysioDoctor="physician";
-
-        console.log(doctorname);
-        console.log(PhysioDoctor);
-
+    doctorDetails(doctorName) {
+        return doctorName;
     }
-
-    doctor2() {
-        const doctorname = "Dr.Suresh"
-        let nueroDoctor="neuroSpecialist";
-
-        console.log(doctorname);
-        console.log(nueroDoctor);
-
-
+    nurseDetails(nurseName) {
+        return nurseName;
     }
-
-    doctor3() {
-        const doctorname = "Dr.Mahesh"
-        let heartDoctor="heartSpecialist";
-
-        console.log(doctorname);
-        console.log(heartDoctor);
-
-    }
-
-    nurse1() {
-        const nurseName = "sunita";
-        console.log(nurseName);
-    }
-
-    nurse2() {
-        const nurseName = "gita";
-        console.log(nurseName);
-
-    }
-
-    nurse3() {
-        const nurseName = "sita";
-        console.log(nurseName);
-
-    }
-
 }
-console.log("-----------------------------------")
+console.log("***********************************")
 let patient1 = new hospital();
 console.log("Patient 1 Details :");
 console.log("===================================");
 console.log("Doctor Assigned to patient 1 :");
-patient1.doctor1();
-
+console.log(patient1.doctorDetails("shyresh"));
 console.log("Nurse Assigned to patient 1 :");
-patient1.nurse1();
+console.log(patient1.nurseDetails("Divya"));
 patient1.patientName = "Mohan";
 patient1.patientAge = 24;
 patient1.patientNumber = 987654321;
 patient1.patientRoomNumber = 101;
-
+patient1.patientStage = "critical";
 
 console.log(patient1);
 
@@ -72,13 +32,16 @@ let patient2 = new hospital();
 console.log("Patient 2 Details :");
 console.log("===================================");
 console.log("Doctor Assigned to patient 2 :");
-patient1.doctor2();
+console.log(patient2.doctorDetails("Rupa"));
+
 console.log("Nurse Assigned to patient 2 :");
-patient1.nurse2();
+console.log(patient2.nurseDetails("Riya"));
+
 patient2.patientName = "Rohan";
 patient2.patientAge = 26;
 patient2.patientNumber = 7987654321;
 patient2.patientRoomNumber = 101;
+patient2.patientStage = "Normal";
 
 console.log(patient2);
 
@@ -86,13 +49,14 @@ let patient3 = new hospital();
 console.log("Patient 3 Details :");
 console.log("===================================");
 console.log("Doctor Assigned to patient 3 :");
-patient1.doctor3();
+console.log(patient3.doctorDetails("Jony"));
 console.log("Nurse Assigned to patient 3 :");
-patient1.nurse3();
+console.log(patient3.nurseDetails("Pooja"));
 patient3.patientName = "Sohan";
 patient3.patientAge = 28;
 patient3.patientNumber = 8123456781;
 patient3.patientRoomNumber = 103;
+patient3.patientStage = "critical";
 
 console.log(patient3);
 
