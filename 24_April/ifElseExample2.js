c23006ca0a10831e55cb12f58886eac922ee35a9
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2024-04-24 10:50:43 +0530

    program

--- 24_April/ifElseExample2.js
+++ 24_April/ifElseExample2.js
@@ -6,5 +6,30 @@ function hospital(medicine){
         return false;
     }
 }
-console.log(hospital("Dolo"));
-console.log(hospital("paracetamol"));
+// console.log(hospital("Dolo"));
+// console.log(hospital("paracetamol"));
+
+//else if example
+
+function cityHospital(medicine){
+    if(medicine==="dolo"){
+        return "dolo is available";
+
+    }
+    else if(medicine==="paracetamol"){
+        return "paracetamol is available"
+    }
+    else if(medicine==="seradon"){
+        return "seradon is available"
+    }
+    else{
+        return "This medicine is not availbale"
+    }
+
+
+}
+
+console.log(cityHospital("dolo"));
+console.log(cityHospital("seradon"));
+console.log(cityHospital("flexon"));
+
