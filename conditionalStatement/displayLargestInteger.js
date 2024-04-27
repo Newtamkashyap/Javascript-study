3d788ece34e6193360b75ae9a3b53d1c587ebf71
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2024-04-27 16:00:20 +0530

    adding conditional statement and loop programs

---
+++ conditionalStatement/displayLargestInteger.js
@@ -0,0 +1,13 @@
+//1. Write a JavaScript program that displays the largest integer among two integers.
+function displayLargestNumber(num1,num2){
+    
+if(num1>num2){
+    console.log(num1,"is greater integer")
+
+}
+else{
+    console.log(num2,"is greater integer")
+}
+
+}
+displayLargestNumber(103,20);
