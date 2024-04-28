2e20db0a0b0f64798acbe3ff0ddcb235c73c39e9
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2024-04-28 17:28:00 +0530

    type

--- functionQuestions.js/checkDataType.js
+++ functionQuestions.js/checkDataType.js
@@ -16,6 +16,17 @@ console.log(typeof(hello({name:"netam",age:24})));
 
 console.log(typeof(hello([1])));
 console.log(typeof(hello()));
+console.log(typeof(hello(function(){})));
+console.log(typeof(hello(null)));
+
+/*NOTE:-
+The data type of NaN is number
+The data type of an array is object
+The typeof operator returns "object" for arrays because in JavaScript arrays are objects.
+The data type of a date is object
+The data type of null is object
+The data type of an undefined variable is undefined *
+The data type of a variable that has not been assigned a value is also undefined  */
 
 
 
