bc26da9dd0ab8e0c3063c3ea0e3cdda4f55caa24
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2024-04-29 13:09:57 +0530

    armstrong

--- conditionalStatement/armstrongNumber.js
+++ conditionalStatement/armstrongNumber.js
@@ -3,7 +3,7 @@
 //120 is not a Armstrong number.
 //1*1*1 + 2*2*2 + 0*0*0 = 9
 function findArmStrongNumber(a,b,c){
-   if(a**3+b**3+c**3===)
+//    if(a**3+b**3+c**3===)
 
     return "is armstrongNumber";
 
